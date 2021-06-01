50095f24a6658a3a1a2b9c243b1afbad860e519e
ffpaiki <74241058+ffpaiki@users.noreply.github.com>
2021-06-02 06:00:01 +0900

    Initial App

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f11e51cb6892b92250a35683a7571a10'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a7104230-c988-4ae4-ae8d-393587f70730'
+};
